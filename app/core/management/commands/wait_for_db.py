92868dbf8004bb8bf410311df3818cfc3bbb433d
Hen Shiryon <hens2013@gmail.com>
2023-08-01 16:52:04 +0300

    adding user handleing

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,18 +5,20 @@ from django.core.management.base import BaseCommand
 from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 import time
+
+
 class Command(BaseCommand):
     """Django command to wait for database."""
 
     def handle(self, *args, **options):
         """entry point for command"""
         self.stdout.write("waiting for database")
-        db_up=False
+        db_up = False
         while not db_up:
             try:
-                self.check(databases=['default']) 
-                db_up=True
-            except(Psycopg2OpError,OperationalError):
+                self.check(databases=['default'])
+                db_up = True
+            except(Psycopg2OpError, OperationalError):
                 self.stdout.write("Database unavaiable, waiting 1 second")
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database avaiable'))
